06e932428c49a0399b1ea4fe497c57467a0b6632
grant zhu <grantzhu5729@163.com>
2019-03-08 23:09:55 +0800

    finish second page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { selfInstroduction } from '../entity/self-introduction';
 
 @Component({
@@ -8,6 +8,22 @@ import { selfInstroduction } from '../entity/self-introduction';
 })
 export class AppComponent {
     title = '朱雪元';
+    randomNum = 2;
     instroductions: string[] = selfInstroduction;
+    //show first tab content
+    firstTabFlag = true;
     construction(){ }
+
+    ngOnInit(){
+	
+    }
+    firstTabSelect(){
+	this.firstTabFlag = true;
+    }
+    thirdTabSelect(){
+	this.firstTabFlag = false;
+	console.log(this.firstTabFlag);
+	setInterval(function(){this.randomNum = Math.floor(Math.random()*2+1);console.log(this.randomNum)},1000)
+    }
+    
 }
--- src/app/personal-skill/personal-skill.component.ts
+++ src/app/personal-skill/personal-skill.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
 import { personalSkill,personalSkills } from '../../entity/personal-skills';
 import * as d3 from 'd3';
 import {ViewEncapsulation} from '@angular/core'
@@ -12,11 +12,11 @@ import {ViewEncapsulation} from '@angular/core'
 export class PersonalSkillComponent implements OnInit {
     skill:personalSkill[]=personalSkills;
     constructor() { }
-    
     ngOnInit() {
-	this.drawBarChart()
+	this.drawBarChart();
     }
-   
+    //this.flag = false;
+    //if used by multiple page
     drawBarChart():void{
 	d3.select("#skill")
 	    .selectAll("div")
